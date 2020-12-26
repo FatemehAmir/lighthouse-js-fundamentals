d43918d2490b69b0dc7c8cfc24bb2fa42380b2c9
FatemehAmir <f.amirkhanloo64@gmai.com>
2020-12-26 06:29:55 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,4 @@
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
+var temperature=45 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
