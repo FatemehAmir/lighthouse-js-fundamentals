290a4e0837d40cd8408faec01edc33039eaec788
FatemehAmir <f.amirkhanloo64@gmai.com>
2020-12-27 09:59:48 +0000

    add new file

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+
+// Step 1: 
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Step 2: 
+console.log(iceCreamFlavours[0]);
+
+// Step 3: 
+var numberOfFlavours = iceCreamFlavours.length;
+console.log(iceCreamFlavours[numberOfFlavours - 1]);
+
+// Step 4: 
+console.log(numberOfFlavours);
